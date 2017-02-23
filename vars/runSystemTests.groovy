ec3238f2a48715652d155ab6c6b089773ff5a265
Ioannis Canellos <iocanel@gmail.com>
2017-02-23 18:58:14 +0200

    Control the system test namespace from the pipeline. Limit the use of $(oc project -q).

--- vars/runSystemTests.groovy
+++ vars/runSystemTests.groovy
@@ -5,18 +5,19 @@ def call(Map parameters = [:]) {
     def component = parameters.get('component')
     def version = parameters.get('version', '1.0')
     def key = "${component}_${version}".toUpperCase().replace('-', '_')
+    def namespace = "systest-${version}"
     
     container(name: 'openshift') {
             git 'https://github.com/redhat-ipaas/ipaas-system-tests.git'
             sh 'echo OPENSHIFT_MASTER: $(oc whoami --show-server) > parameters.yml'
-            sh 'echo OPENSHIFT_OAUTH_CLIENT_ID: system:serviceaccount:$(oc project -q):ipaas-oauth-client >> parameters.yml'
+            sh "echo OPENSHIFT_OAUTH_CLIENT_ID: system:serviceaccount:${namespace}:ipaas-oauth-client >> parameters.yml"
             sh 'echo OPENSHIFT_OAUTH_CLIENT_SECRET: $(oc sa get-token ipaas-oauth-client) >> parameters.yml'
-            sh 'echo OPENSHIFT_OAUTH_DEFAULT_SCOPES: "user:info user:check-access role:edit:$(oc project -q):!" >> parameters.yml'
+            sh "echo OPENSHIFT_OAUTH_DEFAULT_SCOPES: \"user:info user:check-access role:edit:${namespace}:!\" >> parameters.yml"
             sh "echo $key:  $version >> parameters.yml"
             sh 'cat parameters.yml'
         }
 
         container(name: 'maven') {
-            sh 'mvn clean install -Dtemplate.parameters.file=$(pwd)/parameters.yml'
+            sh "mvn clean install -Dtemplate.parameters.file=" + pwd() + "/parameters.yml -Dnamespace.use.existing" + namespace
         }
 }
--- vars/updateComponent.groovy
+++ vars/updateComponent.groovy
@@ -9,10 +9,10 @@ def call(Map parameters = [:]) {
     
      container(name: 'openshift') {
         sh "oc process redhat-ipaas-dev-single-tenant -l component=${component} \
-                -p OPENSHIFT_MASTER=\$(oc whoami --show-server) \
-                -p OPENSHIFT_OAUTH_CLIENT_ID=system:serviceaccount:\$(oc project -q):ipaas-oauth-client \
+                -p OPENSHIFT_MASTER=${namespace} \
+                -p OPENSHIFT_OAUTH_CLIENT_ID=system:serviceaccount:${namespace}:ipaas-oauth-client \
                 -p OPENSHIFT_OAUTH_CLIENT_SECRET=\$(oc sa get-token ipaas-oauth-client) \
-                -p OPENSHIFT_OAUTH_DEFAULT_SCOPES=\"user:info user:check-access role:edit:\$(oc project -q):!\" \
+                -p OPENSHIFT_OAUTH_DEFAULT_SCOPES=\"user:info user:check-access role:edit:${namespace}:!\" \
                 -p ${key}i=${version} | oc update -f -n ${namespace}"
         }
 }
