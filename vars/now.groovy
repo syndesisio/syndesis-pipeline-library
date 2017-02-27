ec848e9e415b0c9bd75dad5e50aaa12091cba919
Ioannis Canellos <iocanel@gmail.com>
2017-02-27 17:31:46 +0200

    Fix now arguments.

--- vars/now.groovy
+++ vars/now.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/groovy
 
-def call(Map parameters = [:], body) {
+def call(Map parameters = [:]) {
     def format = parameters.get('format', 'yyyyMMddHHmm')
     return new java.text.SimpleDateFormat(format).format(new Date())
 }
