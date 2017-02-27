50f7b56d92de0b6455872d7943a8d95a95c696b4
Ioannis Canellos <iocanel@gmail.com>
2017-02-27 18:07:44 +0200

    Make maven local repo volume claim name configurable.

--- vars/withMaven.groovy
+++ vars/withMaven.groovy
@@ -6,15 +6,16 @@ def call(Map parameters = [:], body) {
     def label = parameters.get('label', defaultLabel)
 
     def mavenImage = parameters.get('mavenImage', 'maven:3.3.9')
-    def persistent = parameters.get('persistent', true)
     def inheritFrom = parameters.get('inheritFrom', 'base')
     def serviceAccount = parameters.get('serviceAccount', '')
     def workingDir = parameters.get('workingDir', '/home/jenkins')
+    def mavenRepositoryClaim = parameters.get('mavenRepositoryClaim', '')
+    def persistent = !mavenRepositoryClaim.isEmpty()
 
     if (persistent) {
         podTemplate(label: label, inheritFrom: "${inheritFrom}", serviceAccount: "${serviceAccount}",
                 containers: [containerTemplate(name: 'maven', image: "${mavenImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true, envVars: [containerEnvVar(key: 'MAVEN_OPTS', value: "-Dmaven.repo.local=${workingDir}/.mvnrepository/")])],
-                volumes: [persistentVolumeClaim(claimName: 'm2-local-repo', mountPath: "/${workingDir}/.mvnrepository")]) {
+                volumes: [persistentVolumeClaim(claimName: "${mavenRepositoryClaim}", mountPath: "/${workingDir}/.mvnrepository")]) {
             body()
         }
     } else {
