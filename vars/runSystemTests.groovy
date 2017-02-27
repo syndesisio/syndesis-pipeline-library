c5c700ab4a604335623a3c9d33e24573a5e8cdae
Ioannis Canellos <iocanel@gmail.com>
2017-02-27 20:27:05 +0200

    Try to fix path of parameters.yml.

--- vars/runSystemTests.groovy
+++ vars/runSystemTests.groovy
@@ -7,18 +7,20 @@ def call(Map parameters = [:]) {
     def key = "${component}_VERSION".toUpperCase().replace('-', '_')
     def defaultNamespace = "systest-${version}"
     def namespace = parameters.get('namespace', defaultNamespace)
+    def parametersFilePath = "parameters.yml";
     
     container(name: 'openshift') {
+            parametersFilePath = pwd() + "/parameters.yml"
             git 'https://github.com/redhat-ipaas/ipaas-system-tests.git'
-            sh 'echo OPENSHIFT_MASTER: $(oc whoami --show-server) > parameters.yml'
-            sh "echo OPENSHIFT_OAUTH_CLIENT_ID: system:serviceaccount:${namespace}:ipaas-oauth-client >> parameters.yml"
-            sh 'echo OPENSHIFT_OAUTH_CLIENT_SECRET: $(oc sa get-token ipaas-oauth-client) >> parameters.yml'
-            sh "echo OPENSHIFT_OAUTH_DEFAULT_SCOPES: \"user:info user:check-access role:edit:${namespace}:!\" >> parameters.yml"
-            sh "echo $key:  $version >> parameters.yml"
-            sh 'cat parameters.yml'
+            sh 'echo OPENSHIFT_MASTER: $(oc whoami --show-server) > ' + parametersFilePath
+            sh "echo OPENSHIFT_OAUTH_CLIENT_ID: system:serviceaccount:${namespace}:ipaas-oauth-client >> " + parametersFilePath
+            sh 'echo OPENSHIFT_OAUTH_CLIENT_SECRET: $(oc sa get-token ipaas-oauth-client) >> ' + parametersFilePath
+            sh "echo OPENSHIFT_OAUTH_DEFAULT_SCOPES: \"user:info user:check-access role:edit:${namespace}:!\" >> " + parametersFilePath
+            sh "echo $key:  $version >> " + parametersFilePath
         }
 
         container(name: 'maven') {
-            sh 'mvn clean install -U -Dtemplate.parameters.file=' + pwd() + '/parameters.yml -Dnamespace.use.existing=' + namespace
+            sh 'cat ' + parametersFilePath
+            sh 'mvn clean install -U -Dtemplate.parameters.file=' + parametersFilePath + ' -Dnamespace.use.existing=' + namespace
         }
 }
