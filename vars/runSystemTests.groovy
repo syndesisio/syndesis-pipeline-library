bb92f337015259161d7b5a36b740c62f60fe8881
Ioannis Canellos <iocanel@gmail.com>
2017-03-10 15:36:29 +0200

    runSystemTests now just copies `oc` binary into the workspace and calls maven.

--- vars/runSystemTests.groovy
+++ vars/runSystemTests.groovy
@@ -10,23 +10,11 @@ def call(Map parameters = [:]) {
     def parametersFilePath = "parameters.yml";
     
     container(name: 'openshift') {
-            parametersFilePath = pwd() + "/parameters.yml"
-            git 'https://github.com/redhat-ipaas/ipaas-system-tests.git'
-
-            sh "oc update -f https://raw.githubusercontent.com/redhat-ipaas/openshift-templates/master/serviceaccount-as-oauthclient-single-tenant.yml -n ${namespace}" +
-                    " || " +
-                    "oc create -f https://raw.githubusercontent.com/redhat-ipaas/openshift-templates/master/serviceaccount-as-oauthclient-single-tenant.yml -n ${namespace}"
-            sh "echo OPENSHIFT_MASTER: \$(oc whoami --show-server) > ${parametersFilePath}"
-            sh "echo ROUTE_HOSTNAME: ipaas-staging.b6ff.rh-idev.openshiftapps.com >> ${parametersFilePath}"
-            sh "echo KEYCLOAK_ROUTE_HOSTNAME: ipaas-staging-keycloak.b6ff.rh-idev.openshiftapps.com >> ${parametersFilePath}"
-            sh "echo OPENSHIFT_OAUTH_CLIENT_ID: system:serviceaccount:${namespace}:ipaas-oauth-client >> ${parametersFilePath}"
-            sh "echo OPENSHIFT_OAUTH_CLIENT_SECRET: \$(oc sa get-token ipaas-oauth-client -n ${namespace}) >> ${parametersFilePath}"
-            sh "echo OPENSHIFT_OAUTH_DEFAULT_SCOPES: \"user:info user:check-access role:edit:${namespace}:!\" >> ${parametersFilePath}"
-            sh "echo $key:  $version >> ${parametersFilePath}"
+            //copy the oc binary to the project workspace
+            sh 'cp $(which oc) $(pwd)'
         }
 
         container(name: 'maven') {
-            sh "cat ${parametersFilePath}"
-            sh "mvn clean install -U -Dtemplate.parameters.file=${parametersFilePath} -Dnamespace.use.existing=${namespace}"
+            sh "mvn clean install -U -Dnamespace.use.existing=${namespace}"
         }
 }
