23e86783fba70d1c4119c6366f16567b8c82979a
Ioannis Canellos <iocanel@gmail.com>
2017-03-11 22:15:37 +0200

    Improve runSystemTests function.

--- vars/runSystemTests.groovy
+++ vars/runSystemTests.groovy
@@ -10,11 +10,19 @@ def call(Map parameters = [:]) {
     def parametersFilePath = "parameters.yml";
     
     container(name: 'openshift') {
-            //copy the oc binary to the project workspace
-            sh 'cp $(which oc) $(pwd)'
-        }
+        //copy the oc binary to the project workspace
+        sh """
+        mkdir -p \${HOME}/bin
+        cp \$(which oc) \${HOME}/bin/
+        """
+    }
 
-        container(name: 'maven') {
-            sh "mvn clean install -U -Dnamespace.use.existing=${namespace}"
-        }
+    container(name: 'maven') {
+        git 'https://github.com/redhat-ipaas/ipaas-system-tests.git'
+        sh """
+        mkdir -p \${HOME}/bin
+        export PATH=\${PATH}:\${HOME}/bin
+        mvn clean install -U -Dnamespace.use.existing=${namespace}
+        """
+    }
 }
