917bd2b98ddd3f8eb75838c526bf31088896a7c5
Ioannis Canellos <iocanel@gmail.com>
2017-03-12 23:47:56 +0200

    Use openshiftDeploy inside the rollout function, as oc seem broken atm.

--- vars/rollout.groovy
+++ vars/rollout.groovy
@@ -10,8 +10,9 @@ def call(Map parameters = [:]) {
     def deploymentConfig = parameters.get('deploymentConfig', 'unknown')
     def namespace = parameters.get('namespace', 'ipaas-staging')
 
-
-    container(name: 'openshift') {
-        sh "oc rollout latest ${deploymentConfig} -n ${namespace}"
-    }
+    openshiftDeploy(depCfg: "${deploymentConfig}", namespace: "${namespace}")
+    // The snippet below is broken, as it fails when run inside a pod. Possibly a bug in `oc`.
+    //container(name: 'openshift') {
+    //    sh "oc rollout latest ${deploymentConfig} -n ${namespace}"
+    //}
 }
