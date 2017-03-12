8b3f797ac03b1bc48750a11ff4bd2d20e512731b
Ioannis Canellos <iocanel@gmail.com>
2017-03-12 21:37:44 +0200

    Add a function that helps promoting images from one project to an other.

---
+++ vars/promoteImage.groovy
@@ -0,0 +1,25 @@
+#!/usr/bin/groovy
+
+/**
+ * Promotes an image from one project to the other
+ * @param parameters
+ * @return
+ */
+def call(Map parameters = [:]) {
+
+    def sourceProject = parameters.get('sourceProject', 'ipaas-testing')
+    def targetProject = parameters.get('targetProject', 'ipaas-staging')
+
+    def imageStream = parameters.get('imageStream', 'unknown')
+    def sourceImageStream = parameters.get('sourceImageStream', imageStream)
+    def targetImageStream = parameters.get('targetImageStream', sourceImageStream)
+
+    def tag = parameters.get('tag', 'latest')
+    def sourceTag = parameters.get('sourceTag', tag)
+    def targetTag = parameters.get('targetTag', tag)
+
+
+    container(name: 'openshift') {
+        sh "oc tag ${targetProject}/${targetImageStream}:${targetTag} ${sourceImageStream}:${sourceTag} -n ${sourceProject}"
+    }
+}
